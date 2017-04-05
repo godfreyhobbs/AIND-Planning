061f04eb6567d31f9741e433ef53f48d9281a089
godfreyhobbs <godfreyhobbs@gmail.com>
2017-04-04 21:55:45 -0700

    problem 3

--- my_air_cargo_problems.py
+++ my_air_cargo_problems.py
@@ -274,8 +274,8 @@ def air_cargo_p3() -> AirCargoProblem:
 
     init = FluentState(pos, neg)
     goal = [expr('At(C1, JFK)'),
-            expr('At(C2, JFK)'),
-            expr('At(C3, SFO)'),
+            expr('At(C3, JFK)'),
+            expr('At(C2, SFO)'),
             expr('At(C4, SFO)'),
             ]
     return AirCargoProblem(cargos, planes, airports, init, goal)
